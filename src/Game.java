360262adb05a728e44d0c1ba38ed96a6cac2424e
syzsrh <sarahsyazwina@siswa.um.edu.my>
2020-12-09 00:36:03 +0800

    initial

---
+++ src/Game.java
@@ -0,0 +1,26 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sarahsyazwina
+ */
+public class Game extends JFrame implements ActionListener {
+    
+    public Game(){
+        
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
